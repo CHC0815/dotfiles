6d93344bf8e5e8642c4a3bce5885522f871fe512
Conrad Heinrich Carl <coheca0815@gmail.com>
2023-05-05 17:10:52 +0200

    symlink alacritty

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ DOTFILES=$(pwd)
 ln -s $DOTFILES/nvim $HOME/.config/nvim
 git clone https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
+
+# tmux
+ln -s $DOTFILES/tmux $HOME/.config/tmux
+
+# alacritty
+ln -s $DOTFILES/alacritty $HOME/.config/alacritty
